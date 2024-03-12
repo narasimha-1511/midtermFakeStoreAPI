55c5c3a52b92515932ed76ff609e202a74c343e9
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-03-12 15:31:33 +0530

    updating sto with getters and setters
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

--- src/main/java/dev/narasimha/midterm/dto/FakeStoreProductDto.java
+++ src/main/java/dev/narasimha/midterm/dto/FakeStoreProductDto.java
@@ -1,5 +1,10 @@
 package dev.narasimha.midterm.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FakeStoreProductDto {
     private Long id;
     private String title;
