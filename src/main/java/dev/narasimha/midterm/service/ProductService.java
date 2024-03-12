c52479054b2cdcac3681df75c4dfedd347a7ee10
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-03-12 15:17:24 +0530

    add all methods needed to product service
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

--- src/main/java/dev/narasimha/midterm/service/ProductService.java
+++ src/main/java/dev/narasimha/midterm/service/ProductService.java
@@ -1,2 +1,22 @@
-package dev.narasimha.midterm.service;public interface ProductService {
+package dev.narasimha.midterm.service;
+
+import dev.narasimha.midterm.models.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+
+    Product getSingleProduct(Long id);
+
+    String[] getAllCategories();
+
+    List<Product> getSingleCategories(String category);
+
+    Product addNewProduct(Product product);
+
+    Product UpdateProduct(Long id);
+
+    Product DeleteProduct(Long id);
 }
