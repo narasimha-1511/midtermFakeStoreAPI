0df0c2becdafed51a0f394c3f66718fbd083cb0e
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-03-12 14:56:59 +0530

    Added all the models needed
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

---
+++ src/main/java/dev/narasimha/midterm/models/Product.java
@@ -0,0 +1,20 @@
+package dev.narasimha.midterm.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+
+    private Long id;
+    private String title;
+    private String description;
+    private double price;
+    private Category category;
+    private String imageURL;
+
+
+}
+
