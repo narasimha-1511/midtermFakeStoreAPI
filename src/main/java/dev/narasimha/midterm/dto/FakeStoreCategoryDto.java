22b4cb757f326a87586a3f4af93a7a487d57a184
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-03-12 15:24:19 +0530

    dto's setup complete
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

--- src/main/java/dev/narasimha/midterm/dto/FakeStoreCategoryDto.java
+++ src/main/java/dev/narasimha/midterm/dto/FakeStoreCategoryDto.java
@@ -1,4 +1,10 @@
 package dev.narasimha.midterm.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FakeStoreCategoryDto {
+    private String name;
 }
--- src/main/java/dev/narasimha/midterm/dto/FakeStoreProductDto.java
+++ src/main/java/dev/narasimha/midterm/dto/FakeStoreProductDto.java
@@ -1,4 +1,10 @@
 package dev.narasimha.midterm.dto;
 
 public class FakeStoreProductDto {
+    private Long id;
+    private String title;
+    private double price;
+    private String category;
+    private  String description;
+    private String image;
 }
